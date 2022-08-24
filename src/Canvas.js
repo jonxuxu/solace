cf474e40d672f64eac0b13a19a43f254f7fc517d
Howard Halim <howardhalim@gmail.com>
2022-08-24 17:10:47 -0400

    letters line up

--- src/Canvas.js
+++ src/Canvas.js
@@ -12,34 +12,29 @@ const bigRippleWidth = 2;
 const smallRippleSpeed = 20;
 const smallRippleMaxTime = 3;
 const smallRippleWidth = 2;
-const burstTime1 = 3;
+const burstTime1 = 1;
 const burstTime2 = 3;
-const burstMaxTime = burstTime1 + burstTime2;
-const burstMinRadius = 100;
-const burstMaxRadius = 150;
 
-// Github Copilot writing poetry
 const poem = [
 	"I am a poem",
-	"I am a poem",
-	"I am a poem",
+	"ABCDE",
+	"This is the third line of the poem",
 ];
 
-function Canvas({ awareness }) {
-	// TODO: combine bigRipples, smallRipples, mousePaths, and bursts into:
-	// canvasInfo = {
-	// 	 client1: {
-	//		 bigRipples: [],
-	//		 smallRipples: [],
-	//		 mousePaths: [],
-	//		 bursts: [],
-	//	 },
-	//	 client2: {
-	//	   ...
-	//	 }
-	// }
-	// (stored locally)
+function burstScale1(scale, t) {
+	t = Math.min(t / (burstTime1 + burstTime2), 1);
+	return Math.sqrt(1-(1-t)*(1-t)) * scale;
+}
 
+function burstScale2(t) {
+	if (t < 0)
+		return 0;
+	else
+		return -(Math.cos(Math.PI * t) - 1) / 2;
+		//return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+}
+
+function Canvas({ awareness }) {
   let bigRipples = [];
   let smallRipples = [];
   let mousePaths = {};
@@ -50,7 +45,7 @@ function Canvas({ awareness }) {
   let holdTimer = null;
   const clientId = awareness.clientID;
 
-  const mousePressed = (p5) => {
+  function mousePressed(p5) {
     awareness.setLocalStateField("canvasInfo", {
       smallRipple: {
         x: p5.mouseX,
@@ -75,11 +70,13 @@ function Canvas({ awareness }) {
         holdState = 0;
         const now = Date.now();
         awareness.setLocalStateField("canvasInfo", {
+					/*
           bigRipple: {
             x: p5.mouseX,
             y: p5.mouseY,
             timestamp: Date.now(), // only used to ensure uniqueness
           },
+					*/
           burst: {
             x: p5.mouseX,
             y: p5.mouseY,
@@ -91,12 +88,12 @@ function Canvas({ awareness }) {
     }, 100);
   };
 
-  const mouseReleased = () => {
+  function mouseReleased() {
     clearInterval(holdTimer);
     holdState = 0;
   };
 
-  const mouseMoved = (p5) => {
+  function mouseMoved(p5) {
     awareness.setLocalStateField("canvasInfo", {
       mouse: {
         x: p5.mouseX,
@@ -105,115 +102,143 @@ function Canvas({ awareness }) {
       },
     });
   };
+	
+	function drawLetter(p5, letterInfo, burst, time) {
+		const { letter, index, scale, v1X, v1Y, v2X, v2Y, endTime } = letterInfo;
+		time = Math.min(time, endTime);
+		const a1 = burstScale1(scale, time);
+		//const a2 = burstScale2((time + burstTime2 - endTime) / burstTime2);
+		const a2 = burstScale2(time / endTime);
+		const posX = burst.x + a1 * v1X + a2 * v2X;
+		const posY = burst.y + a1 * v1Y + a2 * v2Y;
+		p5.text(letter, posX, posY);
+	}
 
-  awareness.on("change", ({ updated }) => {
-    if (updated) {
-    	const now = Date.now();
-      const states = awareness.getStates();
-      updated.forEach((clientID) => {
-        const { canvasInfo } = states.get(clientID);
-        if (canvasInfo) {
-          const { smallRipple, bigRipple, burst, mouse } = canvasInfo;
-          if (smallRipple) {
-            smallRipples.push({
-              x: smallRipple.x,
-              y: smallRipple.y,
-              startTime: now,
-            });
-          }
-          // Big ripple location
-          if (bigRipple) {
-            bigRipples.push(
-              { x: bigRipple.x, y: bigRipple.y, startTime: now },
-              { x: bigRipple.x, y: bigRipple.y, startTime: now + 100 },
-              { x: bigRipple.x, y: bigRipple.y, startTime: now + 200 }
-            );
-          }
-					if (burst) {
-						let letters = poem[burst.line].split("").map((letter, index) => {
-							const randomAngle = Math.random() * Math.PI * 2;
-							const radius = burstMinRadius + Math.random() * (burstMaxRadius - burstMinRadius);
-							return {
-								letter,
-								index,
-								midX: burst.x + Math.cos(randomAngle) * radius,
-								midY: burst.y + Math.sin(randomAngle) * radius,
-								endX: 100 + 10 * index,
-								endY: 100 + 20 * burst.line,
-							};
-						}).filter((letter) => {
-							return letter.letter !== " ";
-						});
+	function awarenessUpdate(p5, clientID, canvasInfo) {
+		const { smallRipple, bigRipple, burst, mouse } = canvasInfo;
+		const now = Date.now();
+		if (smallRipple) {
+			smallRipples.push({
+				x: smallRipple.x,
+				y: smallRipple.y,
+				startTime: now,
+			});
+		}
+		// Big ripple location
+		if (bigRipple) {
+			bigRipples.push(
+				{ x: bigRipple.x, y: bigRipple.y, startTime: now },
+				{ x: bigRipple.x, y: bigRipple.y, startTime: now + 100 },
+				{ x: bigRipple.x, y: bigRipple.y, startTime: now + 200 }
+			);
+		}
+		if (burst) {
+			let letters = poem[burst.line].split("").map((letter, index) => {
+				const randomAngle = Math.random() * Math.PI * 2;
+				const scale = 100 + Math.random() * 50;
+				const v1X = Math.cos(randomAngle);
+				const v1Y = Math.sin(randomAngle);
+				const endTime = burstTime1 + Math.random() + burstTime2;
+				const s1 = burstScale1(scale, endTime);
+				const midPosX = burst.x + v1X * s1;
+				const midPosY = burst.y + v1Y * s1;
+				// TODO: set endPos properly
+				const endPosX = 500 + 10 * index;
+				const endPosY = 500 + 20 * burst.line;
+				const v2X = endPosX - midPosX;
+				const v2Y = endPosY - midPosY;
 
-						bursts.push({
-							...burst,
-							letters,
-							startTime: now,
-						});
-					}
+				return {
+					letter,
+					index,
+					scale,
+					v1X,
+					v1Y,
+					v2X,
+					v2Y,
+					endTime,
+				};
+			}).filter((letter) => {
+				return letter.letter !== " ";
+			});
 
-          if (mouse) {
-            if (!cursors[clientID]) {
-              cursors[clientID] = {};
-              function updateMyCursor(point) {
-                cursors[clientID].x = point[0];
-                cursors[clientID].y = point[1];
-              }
-              cursors[clientID].pc = new PerfectCursor(updateMyCursor);
-            }
-            cursors[clientID].pc.addPoint([mouse.x, mouse.y]);
-            cursors[clientID].holdState = mouse.holdState;
+			bursts.push({
+				...burst,
+				letters,
+				startTime: now,
+			});
+		}
 
-            /*
-						if (mousePaths[clientID].length === 0) {
-							mousePaths[clientID] = [{
-								x: mouse.x,
-								y: mouse.y,
-								startTime: Date.now(),
-							}];
-						} else {
-							const path = mousePaths[clientID];
-							const x = p5.mouseX;
-							const y = p5.mouseY;
-							const t = Date.now();
-							while (true) {
-								const lastRipple = path[path.length - 1];
-								const x0 = lastRipple.x;
-								const y0 = lastRipple.y;
-								const t0 = lastRipple.startTime;
-								const dx = x - x0;
-								const dy = y - y0;
-								const dt = t - t0;
-								const d = Math.sqrt(dx * dx + dy * dy);
-								if (dt / 3000  + d / maxPathJumpDistance > 1) {
-									if (d > maxPathJumpDistance) {
-										path.push({
-											x: x0 + dx / d * maxPathJumpDistance,
-											y: y0 + dy / d * maxPathJumpDistance,
-											startTime: t + maxPathJumpDistance / maxPathSpeed,
-										});
-									}
-									lastSmallRipple = { x: x2, y: y2, startTime: t2 };
-								} else {
-									break;
-								}
-							}
+		if (mouse) {
+			if (!cursors[clientID]) {
+				cursors[clientID] = {};
+				function updateMyCursor(point) {
+					cursors[clientID].x = point[0];
+					cursors[clientID].y = point[1];
+				}
+				cursors[clientID].pc = new PerfectCursor(updateMyCursor);
+			}
+			cursors[clientID].pc.addPoint([mouse.x, mouse.y]);
+			cursors[clientID].holdState = mouse.holdState;
+
+			/*
+			if (mousePaths[clientID].length === 0) {
+				mousePaths[clientID] = [{
+					x: mouse.x,
+					y: mouse.y,
+					startTime: Date.now(),
+				}];
+			} else {
+				const path = mousePaths[clientID];
+				const x = p5.mouseX;
+				const y = p5.mouseY;
+				const t = Date.now();
+				while (true) {
+					const lastRipple = path[path.length - 1];
+					const x0 = lastRipple.x;
+					const y0 = lastRipple.y;
+					const t0 = lastRipple.startTime;
+					const dx = x - x0;
+					const dy = y - y0;
+					const dt = t - t0;
+					const d = Math.sqrt(dx * dx + dy * dy);
+					if (dt / 3000  + d / maxPathJumpDistance > 1) {
+						if (d > maxPathJumpDistance) {
+							path.push({
+								x: x0 + dx / d * maxPathJumpDistance,
+								y: y0 + dy / d * maxPathJumpDistance,
+								startTime: t + maxPathJumpDistance / maxPathSpeed,
+							});
 						}
-						*/
-          }
-        }
-      });
-    }
-  });
+						lastSmallRipple = { x: x2, y: y2, startTime: t2 };
+					} else {
+						break;
+					}
+				}
+			}
+			*/
+		}
+	};
 
-  const setup = (p5, canvasParentRef) => {
+  function setup(p5, canvasParentRef) {
     let width = canvasParentRef.offsetWidth;
     let height = canvasParentRef.offsetHeight;
     let cnv = p5.createCanvas(width, height).parent(canvasParentRef);
     p5.ellipseMode(p5.RADIUS);
+		awareness.on("change", ({ updated }) => {
+			if (updated) {
+				const states = awareness.getStates();
+				updated.forEach((clientID) => {
+					const canvasInfo = states.get(clientID).canvasInfo;
+					if (canvasInfo) {
+						awarenessUpdate(p5, clientID, canvasInfo);
+					}
+				});
+			}
+		});
   };
 
-  const draw = (p5) => {
+  function draw(p5) {
     const now = Date.now();
     p5.background(0);
 
@@ -256,20 +281,15 @@ function Canvas({ awareness }) {
 
     p5.fill(255);
 		p5.noStroke();
-		bursts = bursts.filter(({ startTime }) => {
-			return (now - startTime) / 1000 < burstMaxTime;
-		});
     bursts.forEach((burst) => {
       const time = (now - burst.startTime) / 1000;
-			if (time < burstTime1) {
-				burst.letters.forEach((letter) => {
-					drawLetter1(p5, letter, burst.x, burst.y, time);
-				});
-			}
+			burst.letters.forEach((letter) => {
+				drawLetter(p5, letter, burst, time);
+			});
     });
   };
 
-  const drawBigRipple = (p5, ripple, time) => {
+  function drawBigRipple(p5, ripple, time) {
     let buffer = cursorRadius + bigRippleWidth / 2;
     let radius = time * bigRippleSpeed + buffer;
     let alpha = p5.map(time, 0, bigRippleMaxTime, cursorAlpha, 0);
@@ -277,7 +297,7 @@ function Canvas({ awareness }) {
     p5.ellipse(ripple.x, ripple.y, radius);
   };
 
-  const drawSmallRipple = (p5, ripple, time) => {
+  function drawSmallRipple(p5, ripple, time) {
     let buffer = cursorRadius + smallRippleWidth / 2;
     let radius = time * smallRippleSpeed + buffer;
     let alpha = p5.map(time, 0, smallRippleMaxTime, cursorAlpha, 0);
@@ -308,15 +328,6 @@ function Canvas({ awareness }) {
 		}
 	}
 	*/
-	
-	const drawLetter1 = (p5, letter, startX, startY, time) => {
-		let t = time / burstTime1;
-		t = Math.sqrt(t);
-		const x = startX + (letter.midX - startX) * t;
-		const y = startY + (letter.midY - startY) * t;
-		console.log(x, y);
-		p5.text(letter.letter, x, y);
-	}
 
 
   return (
